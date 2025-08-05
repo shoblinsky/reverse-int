0bee32235a6af70e8cb72efb2caf8d1435ee2fac
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-05 11:30:01 +0500

    feat: make task roughly

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 module.exports = function reverse(n) {
+  let initString = n.toString
+  let result = '';
+  let i = 0;
+  while (i < initString.length) {
+    result = `${result}${initString[i]}`
+    i += 1
+  }
+  return result
 };
