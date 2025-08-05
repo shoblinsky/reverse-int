c4a30666a87ef2783e1ff7c0265a3d894f1e7558
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-05 11:55:11 +0500

    fix: cycle, fix below zero cases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function reverse(n) {
-  const initString = n.toString();
+  const nAbs = Math.abs(n)
+  const initString = nAbs.toString();
   let result = '';
-  for (let i = 0; i < initString.length; i += i) {
+  for (let i = 0; i < initString.length; i += 1) {
     result = `${initString[i]}${result}`;
   }
   return result;
