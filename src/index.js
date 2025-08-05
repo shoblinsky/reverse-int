9444fcab23b01f85c36259b7aa56c95477cb68a6
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-05 11:55:42 +0500

    fix: linter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse(n) {
-  const nAbs = Math.abs(n)
+  const nAbs = Math.abs(n);
   const initString = nAbs.toString();
   let result = '';
   for (let i = 0; i < initString.length; i += 1) {
